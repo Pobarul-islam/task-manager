eb37b513d125d06af639fdb916d39396e2319340
Pobarul-islam <pobarul3@gmail.com>
2022-07-01 13:07:48 +0600

    navbar removed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import "./Navbar.css";
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div className="Navbar">
-      <span className="nav-logo">Task-Management</span>
-      <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="/home">Completed Tasks</a>
-        <a href="/about">To-Do</a>
-        <a href="/service">Calendar</a>
+// import React, { useState } from "react";
+// import "./Navbar.css";
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   return (
+//     <div className="Navbar">
+//       <span className="nav-logo">Task-Management</span>
+//       <div className={`nav-items ${isOpen && "open"}`}>
+//         <a href="/home">Completed Tasks</a>
+//         <a href="/about">To-Do</a>
+//         <a href="/service">Calendar</a>
        
-      </div>
-      <div
-        className={`nav-toggle ${isOpen && "open"}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="bar"></div>
-      </div>
-    </div>
-  );
-};
+//       </div>
+//       <div
+//         className={`nav-toggle ${isOpen && "open"}`}
+//         onClick={() => setIsOpen(!isOpen)}
+//       >
+//         <div className="bar"></div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
